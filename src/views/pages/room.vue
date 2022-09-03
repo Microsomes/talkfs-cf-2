ecf59ea05ee3a5678cf6a44b1fcbb6cff1af34b4
Tayyab Javed <tayyab.javed@grv.media>
2022-09-03 09:03:48 +0100

    lt

--- src/views/pages/room.vue
+++ src/views/pages/room.vue
@@ -1,11 +1,13 @@
 <template>
     <div>
-       <div v-if="!isSelected" @click="goHome" class="h-12 cursor-pointer flex  bg-gradient-to-r from-green-300   items-center justify-center text-3xl bg-black text-white w-full">
+       <div @click="goHome" class="h-12 cursor-pointer flex  bg-gradient-to-r from-green-300   items-center justify-center text-3xl bg-black text-white w-full">
             <p>TalkFS.com</p>
         </div>
-        <div v-if="!isSelected" class="h-12 flex items-center justify-center text-3xl bg-black text-white w-full">
-            <p class="uppercase"> {{roomID}} </p>
+        <div  class="h-12 flex items-center justify-center text-3xl bg-black text-white w-full">
+            <p class="uppercase"> #{{roomID}} ( Users {{totalConnectedToRoom}} ) </p>
         </div>
+
+        
 
 
         <div class="h-screen  flex flex-col bg-white border-1 border-2 w-96 fixed  right-0">
@@ -22,18 +24,72 @@
 
 <script>
 
+    var socket;
+
+
     export default {
         name: 'RoomView',
         components: {
         },
         created() {
-            this.roomID = this.$route.params.id
+            this.roomID = this.$route.params.id;
+
+            const ISDEV = true;
+
+            socket = new WebSocket("wss://talkfssocket.herokuapp.com");
+
+            if(ISDEV){
+            socket = new WebSocket("ws://localhost:3000");
+            }
+
+            socket.onmessage= (msg)=>{
+            try{
+                const parsedMessage = JSON.parse(msg.data.toString());
+
+                console.log(parsedMessage);
+
+                if(parsedMessage.type == 'joinRoom'){
+                    this.totalConnectedToRoom = parsedMessage.data.connectedPeople;
+                }
+
+                if(parsedMessage.type == 'connectedToRoom'){
+                    this.totalConnectedToRoom = parsedMessage.data
+                }
+                
+            }catch(e){
+                //console.log(e);
+            }
+            }
+
+            //when socket is ready
+            socket.onopen = () => {
+            console.log("Socket is ready");
+            socket.send(JSON.stringify({
+                type: "joinRoom",
+                data:{
+                    roomName: this.roomID
+                }
+            }));
+
+            }
+
+        
+
+
+
+        },
+        beforeRouteLeave(to, from, next) {
+            //CLOSE SOCKET
+            socket.close();
+            next();
         },
         mounted() {
         },
         data() {
             return {
                 roomID: "",
+                totalConnectedToRoom: 0,
+                message:''
             }
         },
         methods: {
